6711db84a89b0db514984f7dd4abe7b3551bb6de
Anthony <amazzolaphoto@gmail.com>
2019-08-16 18:36:15 +0000

    trying to make places photos work

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_30_192134) do
+ActiveRecord::Schema.define(version: 2019_08_16_183345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2019_07_30_192134) do
     t.integer "user_id"
     t.float "latitude"
     t.float "longitude"
+    t.string "photo"
     t.index ["user_id"], name: "index_places_on_user_id"
   end
 
